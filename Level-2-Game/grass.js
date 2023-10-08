e1f46ab6a3980ce5999d995bbcb0e9bcaeadedfa
dmartirosyan1 <diana.martirosyan1@tumo.org>
2023-10-08 13:23:47 +0400

    class extends

---
+++ Level-2-Game/grass.js
@@ -0,0 +1,24 @@
+class Grass extends LivingCreature {
+    constructor() {
+     super()
+     this.multiply = 0;
+    } 
+
+        mul() {
+            this.multiply++;
+            var emptyCells = this.chooseCell(0);
+            var newCell = random(emptyCells);
+            if (newCell && this.multiply >= 8) {
+                var newX = newCell[0];
+                var newY = newCell[1];
+                matrix[newY][newX] = 1;
+
+                var newGrass = new Grass(newX, newY, 1);
+                grassArr.push(newGrass);
+                this.multiply = 0;
+            }
+        }
+
+    }
+
+
